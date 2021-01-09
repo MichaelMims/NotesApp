d85ff86560f9b2eb5e994d2698be654b2cdad6fd
MichaelMims <MichaelMims@users.noreply.github.com>
2021-01-09 14:00:57 -0600

    v1.0.7
    Added docker compose file.
    
    Changed way back-end connects to database. Now retries every 5 seconds to connect

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,7 +1,7 @@
 import express, { Router } from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
-import notesRoutes from './routes/notes'
+import notesRoutes, { connect } from './routes/notes'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
@@ -11,13 +11,25 @@ const app = express()
 const PORT = process.env.PORT || 3001
 
 if (process.env.MONGOOSE_ADDRESS) {
-    mongoose.connect(process.env.MONGOOSE_ADDRESS, { useNewUrlParser: true })
-        .then((res) => {
-            startServer()
-        })
-        .catch((err) => {
-            console.log(err)
-        })
+
+    connectWithRetry(process.env.MONGOOSE_ADDRESS)
+    
+    // var db = mongoose.createConnection(process.env.MONGOOSE_ADDRESS, {})
+    // db.on('error', () => {
+
+    //     console.error('Attempting connection to database')
+    // });
+
+    // db.once('open', () => {
+    //     startServer()
+    // })
+    // mongoose.connect(process.env.MONGOOSE_ADDRESS, { useNewUrlParser: true })
+    //     .then((res) => {
+    //         startServer()
+    //     })
+    //     .catch((err) => {
+    //         console.log(err)
+    //     })
 } else {
     console.log('Could not connect to database')
 }
@@ -32,5 +44,16 @@ function startServer() {
 
     app.listen(PORT, () => {
         console.log(`Listening on port:${PORT}`)
+    })
+}
+
+function connectWithRetry(mongourl: string){
+    return mongoose.connect(mongourl, (err) => {
+        if (err) {
+            console.error('Failed to connect to mongo on startup - retrying');
+            setTimeout(() => {connectWithRetry(mongourl)}, 5000);
+        } else {
+            startServer()
+        }
     })
 }
